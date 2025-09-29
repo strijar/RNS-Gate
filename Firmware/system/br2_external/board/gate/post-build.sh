18696e84bd97527c1afb488736e9663a79d9a201
Belousov Oleg <belousov.oleg@gmail.com>
2025-09-29 16:19:20 +0300

    Kernel dts and config

---
+++ Firmware/system/br2_external/board/gate/post-build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# genimage will need to find the extlinux.conf
+# in the binaries directory
+
+BOARD_DIR="${BR2_EXTERNAL_GATE_PATH}/board/gate"
+
+install -m 0644 -D "${BOARD_DIR}/extlinux.conf" "${TARGET_DIR}/boot/extlinux/extlinux.conf"
