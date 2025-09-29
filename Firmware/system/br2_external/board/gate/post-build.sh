02bbff17ff5dee660c8c9c23e760a00f0021bd19
Belousov Oleg <belousov.oleg@gmail.com>
2025-09-29 21:03:41 +0300

    Fixes. Add rns package. Update config

--- Firmware/system/br2_external/board/gate/post-build.sh
+++ Firmware/system/br2_external/board/gate/post-build.sh
@@ -6,3 +6,4 @@
 BOARD_DIR="${BR2_EXTERNAL_GATE_PATH}/board/gate"
 
 install -m 0644 -D "${BOARD_DIR}/extlinux.conf" "${TARGET_DIR}/boot/extlinux/extlinux.conf"
+install -m 0644 -D "${BOARD_DIR}/genimage.cfg" "$BINARIES_DIR/genimage.cfg"
