f04511192ce1ea7809f1a2f47a44ac05beb78be7
Belousov Oleg <belousov.oleg@gmail.com>
2025-10-31 13:09:02 +0300

    Switch to Buildroot 2024.02

--- Firmware/system/config.sh
+++ Firmware/system/config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-BR_RELEASE="buildroot-2025.05"
+BR_RELEASE="buildroot-2024.02"
 
 if [ ! -e ${BR_RELEASE}.tar.gz ]; then
   wget https://buildroot.org/downloads/${BR_RELEASE}.tar.gz
