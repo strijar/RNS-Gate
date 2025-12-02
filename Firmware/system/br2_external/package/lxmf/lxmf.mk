ba645c790f24204384b4fece7ba2e1680623e227
Belousov Oleg <belousov.oleg@gmail.com>
2025-12-02 19:30:24 +0300

    Update rns and lxmf package

--- Firmware/system/br2_external/package/lxmf/lxmf.mk
+++ Firmware/system/br2_external/package/lxmf/lxmf.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-LXMF_VERSION = 0.8.0
-LXMF_SOURCE = lxmf-$(LXMF_VERSION).tar.gz
-LXMF_SITE = https://files.pythonhosted.org/packages/85/e8/89d7919514ef4edbcee3742084bde6ce06a0f1be5ca6b7c2bb8a2eaae04a
+LXMF_VERSION = 0.9.3
+LXMF_SITE = $(call github,markqvist,LXMF,$(LXMF_VERSION))
 LXMF_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
--- Firmware/system/br2_external/package/rns/rns.mk
+++ Firmware/system/br2_external/package/rns/rns.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-RNS_VERSION = 1.0.0
-RNS_SOURCE = rns-$(RNS_VERSION).tar.gz
-RNS_SITE = https://files.pythonhosted.org/packages/d9/17/c7b056c8c7f2d54d026c07470d6986aa71408677436cb5049e96f43b5cac
+RNS_VERSION = 1.0.4
+RNS_SITE = $(call github,markqvist,Reticulum,$(RNS_VERSION))
 RNS_SETUP_TYPE = setuptools
 RNS_DEPENDENCIES = python-cryptography
 
