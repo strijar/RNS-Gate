d2c9aa0c5fe0e519b3b9fd761e2595c3252bb11b
Belousov Oleg <belousov.oleg@gmail.com>
2026-01-23 12:29:32 +0300

    WiFi fix

--- Firmware/system/br2_external/board/gate/post-build.sh
+++ Firmware/system/br2_external/board/gate/post-build.sh
@@ -10,3 +10,9 @@ install -m 0644 -D "${BOARD_DIR}/genimage.cfg" "$BINARIES_DIR/genimage.cfg"
 
 rm -rf "${BINARIES_DIR}/rns"
 cp -r "${TARGET_DIR}/mnt/rns/" "${BINARIES_DIR}/rns"
+
+# WiFi fix
+
+if [ -f "${TARGET_DIR}/etc/init.d/S10udev" ]; then
+    mv "${TARGET_DIR}/etc/init.d/S10udev" "${TARGET_DIR}/etc/init.d/S46udev"
+fi
