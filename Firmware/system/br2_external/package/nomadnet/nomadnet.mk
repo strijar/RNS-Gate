5bb7122e9cd1719dfc4968600940361f919b0ccd
Belousov Oleg <belousov.oleg@gmail.com>
2026-01-23 12:44:51 +0300

    Update rns and nomadnet packages

--- Firmware/system/br2_external/package/nomadnet/nomadnet.mk
+++ Firmware/system/br2_external/package/nomadnet/nomadnet.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-NOMADNET_VERSION = 0.9.2
+NOMADNET_VERSION = 0.9.3
 NOMADNET_SITE = $(call github,markqvist,NomadNet,$(NOMADNET_VERSION))
 NOMADNET_SETUP_TYPE = setuptools
 
--- Firmware/system/br2_external/package/rns/rns.mk
+++ Firmware/system/br2_external/package/rns/rns.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RNS_VERSION = 1.0.4
+RNS_VERSION = 1.1.3
 RNS_SITE = $(call github,markqvist,Reticulum,$(RNS_VERSION))
 RNS_SETUP_TYPE = setuptools
 RNS_DEPENDENCIES = python-cryptography
