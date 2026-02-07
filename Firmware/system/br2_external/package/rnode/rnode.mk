7047459ed1ec239e5d81b1cc491fa67777e8a441
Belousov Oleg <belousov.oleg@gmail.com>
2026-02-07 21:12:31 +0300

    Update RNode

--- Firmware/system/br2_external/package/rnode/rnode.mk
+++ Firmware/system/br2_external/package/rnode/rnode.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RNODE_VERSION = 878840f141a624880bf2f04a1d9e9556c5ddbfb7
+RNODE_VERSION = 4802f5196e401655ecff55163f32c1e7e426be89
 RNODE_SITE = https://github.com/strijar/RNode-linux
 RNODE_SITE_METHOD = git
 RNODE_LICENSE = GPLv2
